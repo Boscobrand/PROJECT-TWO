962e449d79ad08a3a6538a72fcbc2b90c880a218
Boscobrand <Tony.Bosco@boscobrand.com>
2022-04-19 11:48:09 -0400

    update

--- tb-beast/src/App.js
+++ tb-beast/src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
-    </div>
-  );
+const App = () => {
+    const APP_ID = "59afb331"
+    const APP_KEY = "b2ac96d2f234e2da44353b847332685e"
+    
+    const url = 'https://api.edamam.com/search?q=chicken&app_id=' + APP_ID + "app_key=" + APP_KEY;
+
+    const getData = async () => {
+        const result = await Axios.get(url);
+        console.log(result)
+    }
+
+    return(
+        <div className="APP">
+            <h1 onClick={getData}> API Development Test</h1>
+        </div>
+    );
 }
-
 export default App;
--- tb-beast/src/Components/foodTestAPI1.js
+++
@@ -1,9 +0,0 @@
-import react from "react"
-import Axios from "axios"
-
-const APP = () => {
-    const APP_ID - "59afb331"
-    const APP_KEY = "b2ac96d2f234e2da44353b847332685e"
-
-    const url_1 = `?`
-}
