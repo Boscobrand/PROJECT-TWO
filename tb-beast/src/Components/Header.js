1fe83314a92054e5a1bfdc8876adc9b8ca50d6cf
Boscobrand <Tony.Bosco@boscobrand.com>
2022-04-21 10:14:59 -0400

    header

--- tb-beast/src/Components/Header.js
+++ tb-beast/src/Components/Header.js
@@ -10,18 +10,18 @@ function Header () {
             </div>
 
             <div className="Subhead">
-                <h2> Recipe recommendations based on your choice of wine.</h2>
-                <h2 >Pair wine and food with the push of a button. </h2>
+                <h2 id="A"> Recipe recommendations based on your choice of wine.</h2>
+                <h2 id="B" >Pair wine and food with the push of a button. </h2>
             </div>
 
             <div className="Lefthand">
-                <h3> Wine that pairs well</h3>
-                <h3> with the recipes on the right...</h3>
+                <h3 id="C"> Wine that pairs well</h3>
+                <h3 id="D"> with the recipes on the right...</h3>
             </div>
 
             <div className="Righthand">
-                 <h3> Food that pairs so well with wine</h3>
-                 <h3> there will be nothing left...</h3>
+                 <h3 id="E"> Food that pairs so well with wine</h3>
+                 <h3 id="F"> there will be nothing left...</h3>
             </div>
         </div>
     )
